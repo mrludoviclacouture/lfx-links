42fddb769403557e67fe4b7d7e9956c5f500354f
Ludovic Lacouture <mrludoviclacouture@gmail.com>
2025-09-25 03:36:38 +0200

    seventh commit

--- app/links.js
+++ app/links.js
@@ -7,17 +7,17 @@ const links = [
     on: true
   },
   {
-    title: 'YouTube',
-    url: 'https://m.youtube.com/@litflowx?sub_confirmation=1',
-    type: 'main',
-    icon: '/brand-youtube.svg',
-    on: true
-  },
-  {
     title: 'Instagram',
     url: 'https://www.instagram.com/litflowx',
     type: 'main',
     icon: '/brand-instagram.svg',
+    on: true
+  },
+  {
+    title: 'YouTube',
+    url: 'https://m.youtube.com/@litflowx?sub_confirmation=1',
+    type: 'main',
+    icon: '/brand-youtube.svg',
     on: true
   },
   {
